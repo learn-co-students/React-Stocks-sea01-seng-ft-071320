152c0e0043f93f7a5e6c7223959c6820a363318b
Alec DuBois <after.alec@gmail.com>
2020-09-15 07:51:16 -0700

    Done.

--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
-import Stock from '../components/Stock'
+import React, { Component } from "react";
+import Stock from "../components/Stock";
 
 class StockContainer extends Component {
-
   render() {
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        {this.props.stocks
+          .filter((stock) => stock.type === this.props.filterBy)
+          .sort(this.props.sortBy)
+          .map((stock) => (
+            <Stock
+              key={stock.id}
+              {...stock}
+              handleClick={this.props.buyStock}
+            />
+          ))}
       </div>
     );
   }
-
 }
 
 export default StockContainer;
