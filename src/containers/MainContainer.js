2ce8d9ab982a30504b0b2633cabb3232b4db09cc
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-09-19 13:17:20 -0700

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,24 +5,110 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    stocks: [],
+    portfolio: [],
+    filterBy: [],
+    sortName: false,
+    sortPrice: false,
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/stocks')
+      .then(res => res.json())
+      .then(stocks => this.setState({ stocks: stocks }))
+    // .then(stocks => console.log(stocks))
+  }
+
+  handleClick = (stock) => {
+    if (this.state.portfolio.includes(stock)) {
+      this.setState({
+        portfolio: this.state.portfolio.filter((item) => item !== stock)
+      })
+    } else {
+      this.setState({ portfolio: [...this.state.portfolio, stock] })
+    }
+  }
+
+  handelFilterChange = (e) => {
+    this.setState({ filterBy: e.target.value });
+  }
+  handleClickSortByName = () => {
+    this.setState({ sortName: !this.state.sortName })
+  }
+  handleClickSortByPrice = () => {
+    this.setState({ sortPrice: !this.state.sortPrice })
+
+  }
+
+  stockList = () => {
+    return this.state.stocks.filter((stock) => this.state.portfolio.indexOf(stock) === -1)
+  }
+
+  filterStockList = () => {
+    if (this.state.filterBy.length > 0) {
+      if (this.state.filterBy !== 'All') {
+        return this.stockList().filter((stock) => stock.type === this.state.filterBy)
+      }
+      return this.stockList()
+    }
+    return this.stockList()
+  }
+
+  SortByName = () => {
+    if (this.state.sortName) {
+      return this.filterStockList().sort(function (a, b) {
+        let nameA = a.ticker
+        let nameB = b.ticker
+        if (nameA < nameB) {
+          return -1;
+        }
+        if (nameA > nameB) {
+          return 1;
+        }
+        return 0;
+      })
+    }
+    return this.filterStockList()
+  }
+  sortByPrice = () => {
+    if (this.state.sortPrice) {
+      return this.SortByName().sort(function (a, b) {
+        return a.price - b.price;
+      });
+    }
+    return this.SortByName()
+  }
+
+
+
+
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar
+          filterBy={this.state.filterBy}
+          handelFilterChange={this.handelFilterChange}
+          handleClickSortByName={this.handleClickSortByName}
+          sortName={this.state.sortName}
+          sortPrice={this.state.sortPrice}
+          handleClickSortByPrice={this.handleClickSortByPrice}
+        />
 
-          <div className="row">
-            <div className="col-8">
+        <div className="row">
+          <div className="col-8">
 
-              <StockContainer/>
+            <StockContainer stocks={this.sortByPrice()} handleClick={this.handleClick} />
 
-            </div>
-            <div className="col-4">
-
-              <PortfolioContainer/>
-
-            </div>
           </div>
-      </div>
+          <div className="col-4">
+
+            <PortfolioContainer portfolio={this.state.portfolio} handleClick={this.handleClick} />
+
+          </div>
+        </div>
+      </div >
     );
   }
 
