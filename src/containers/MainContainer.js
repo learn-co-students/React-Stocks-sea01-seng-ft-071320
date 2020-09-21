5ef0047c6eece9e52731607e324f1ddabf6613cd
Nathan <chung.nathanh@gmail.com>
2020-09-21 16:55:37 -0700

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,31 +1,37 @@
-import React, { Component } from 'react';
-import StockContainer from './StockContainer'
-import PortfolioContainer from './PortfolioContainer'
-import SearchBar from '../components/SearchBar'
+import React, { Component } from "react";
+import StockContainer from "./StockContainer";
+import PortfolioContainer from "./PortfolioContainer";
+import SearchBar from "../components/SearchBar";
 
 class MainContainer extends Component {
+  state = {
+    stocks: [],
+    portfolio: [],
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/stocks")
+      .then((res) => res.json())
+      .then((stocks) => this.setState({ stocks: stocks }));
+  }
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar />
 
-          <div className="row">
-            <div className="col-8">
-
-              <StockContainer/>
-
-            </div>
-            <div className="col-4">
-
-              <PortfolioContainer/>
-
-            </div>
+        <div className="row">
+          <div className="col-8">
+            <StockContainer stocks={this.state.stocks} />
           </div>
+          <div className="col-4">
+            {/* filtered stocks go into portfolio container. */}
+            <PortfolioContainer selectStock={this.selectStock} />
+          </div>
+        </div>
       </div>
     );
   }
-
 }
 
 export default MainContainer;
