64f8579ca1fd300d58ecca8134d66b3adb0c2122
Nathan <chung.nathanh@gmail.com>
2020-09-22 20:19:07 -0700

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
-import StockContainer from './StockContainer'
-import PortfolioContainer from './PortfolioContainer'
-import SearchBar from '../components/SearchBar'
+import React, { Component } from "react";
+import StockContainer from "./StockContainer";
+import PortfolioContainer from "./PortfolioContainer";
+import SearchBar from "../components/SearchBar";
 
 class MainContainer extends Component {
-
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar
+          changeFilter={this.props.changeFilter}
+          sortStocks={this.props.sortStocks}
+        />
 
-          <div className="row">
-            <div className="col-8">
-
-              <StockContainer/>
-
-            </div>
-            <div className="col-4">
-
-              <PortfolioContainer/>
-
-            </div>
+        <div className="row">
+          <div className="col-8">
+            <StockContainer
+              stocks={this.props.stocks}
+              addCard={this.props.addCard}
+            />
           </div>
+          <div className="col-4">
+            <PortfolioContainer
+              portfolio={this.props.portfolio}
+              removeCard={this.props.removeCard}
+            />
+          </div>
+        </div>
       </div>
     );
   }
-
 }
 
 export default MainContainer;
